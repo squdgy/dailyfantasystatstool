09e27c443c22ab0dfde259cfe2d3c23fd7f81595
Maura Wilder <maura@devpartners.com>
2012-07-04 07:37:32 -0400

    fixed filter bug when changing dates

--- app/controller/Filters.js
+++ app/controller/Filters.js
@@ -108,6 +108,7 @@ Ext.define('DFST.controller.Filters', {
 
     changeDate: function(datefield, newValue, oldValue, options) {
         var statsStore = this.getStatsStore();
+        statsStore.filters.removeAtKey('team'); // clear all game filters
         statsStore.filter([{id: 'gameDate', property: 'gameDate', value: newValue.toJSON()}]);
         var gamesStore = this.getGamesStore();
         gamesStore.filter([{id:'gameDate', property: 'gameDate', value: newValue.toJSON()}]);
