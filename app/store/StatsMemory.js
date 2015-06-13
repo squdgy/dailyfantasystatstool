2425b59fc972b5f7cfb3b90c852140a8a2c16837
Maura Wilder <maura@devpartners.com>
2015-06-13 14:57:30 +0000

    minor stuff

---
+++ app/store/StatsMemory.js
@@ -0,0 +1,25 @@
+/*global Ext: false, DFST: false */
+/*
+Copyright (c) 2012-2015 Maura Wilder
+*/
+Ext.define('DFST.store.StatsMemory', {
+    extend: 'Ext.data.Store',
+
+    requires: ['Ext.data.reader.Json', 'Ext.data.proxy.Memory'],
+
+    model: 'DFST.model.StatSet',
+
+    autoLoad: false,
+    remoteSort: true,
+    remoteFilter: true,
+    pageSize: 20,
+    sorters: [{
+        property : 'sal',
+        direction: 'DESC'
+    }],
+    proxy: {
+		type: 'memory',
+		enablePaging: true
+	}	
+});
+
